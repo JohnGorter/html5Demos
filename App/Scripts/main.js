0b7298ee197cc00f6c4f639f7b9308c2148dec5c
Boss <john.gorter@gmail.com>
2014-03-03 16:09:18 +0100

    'Demo3-ToDoJavaScriptv2'

---
+++ App/Scripts/main.js
@@ -0,0 +1,20 @@
+function newTodo() {
+    // get the title
+    var title = document.getElementById("title");
+    var ul = document.getElementById ("todolist_ul");
+    // create a new <li>title</li>
+    var li = document.createElement("li");
+    li.innerText = title.value;
+    // add the <li> to the <ul>
+    ul.appendChild(li); 
+}
+
+function setDetails() {
+    var li = window.event.target;
+    var title = document.getElementById("detail_title");
+    //var descr = document.getElementById("detail_descr"); 
+    title.innerText = li.innerText;
+}
+
+
+
