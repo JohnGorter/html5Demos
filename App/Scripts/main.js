2401883701cda689a1b77805932ee94039869042
Boss <john.gorter@gmail.com>
2014-03-04 15:23:38 +0100

    Demos4_BootstrapStep3

--- App/Scripts/main.js
+++ App/Scripts/main.js
@@ -1,14 +1,17 @@
 function newTodo() {
     // get the title
     var title = document.getElementById("title");
+    var badge = document.getElementById("badge");
+    var alertpanel = document.getElementById("alertpanel");
+    var counter = parseInt(badge.innerText);
     var ul = document.getElementById ("todolist_ul");
     // create a new <li>title</li>
     var li = document.createElement("li");
     li.innerText = title.value;
+    badge.innerText = (++counter).toString();
     // add the <li> to the <ul>
+    alertpanel.innerHTML = '<div class="alert alert-info"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><strong>Information!</strong> A new todo has been added!</div>';
     ul.appendChild(li);
-    event.preventDefault();
-    event.stopPropagation();
 }
 
 function setDetails() {
