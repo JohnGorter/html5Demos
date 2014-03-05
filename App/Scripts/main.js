f43d7d3e7c2c8bd10a4e5ebf565419481dea5f95
Boss <john.gorter@gmail.com>
2014-03-05 15:46:09 +0100

    'Demos6_jQueryStep1v3'

--- App/Scripts/main.js
+++ App/Scripts/main.js
@@ -1,28 +1,39 @@
-function newTodo() {
-    /*
-    // get the title
-    var title = document.getElementById("title");
-    var badge = document.getElementById("badge");
-    var alertpanel = document.getElementById("alertpanel");
-    var counter = parseInt(badge.innerText);
-    var ul = document.getElementById ("todolist_ul");
-    // create a new <li>title</li>
-    var li = document.createElement("li");
-    li.innerText = title.value;
-    badge.innerText = (++counter).toString();
-    // add the <li> to the <ul>
-    alertpanel.innerHTML = '<div class="alert alert-info"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><strong>Information!</strong> A new todo has been added!</div>';
-    ul.appendChild(li);
-}
-
-function setDetails() {
-    var li = window.event.target;
-    var title = document.getElementById("detail_title");
-    //var descr = document.getElementById("detail_descr"); 
-    title.innerText = li.innerText;
-}
-*/
-}
-
-
-
+$(function(){
+   // set the badge counter to the number of LI items
+   $("#badge").text($("#tul li").size());
+   // set click function of the panel to hide the panel
+   $("#btnAlert").click(function(){ $("#aPanel").hide()});
+   // add a new todo item to the unordered list
+   $("#bS").click(function() {
+       $("<li>").append(
+           // append a <span> to the LI containing the title of the title input
+           $("<span>").addClass("t").text($("#t").val())
+       ).append(
+           // append a <span> to the LI containing the description of the description input
+           $("<span>").addClass("h").text($("#d").val())
+       ).append(
+           // append a <span> for the done icon and handle the done click 
+           $("<span>").addClass("d glyphicon glyphicon-ok").click(function(){
+               $(this).parent().fadeOut("slow", function() {
+                   // remove the parent LI after the LI has fully faded out
+                   $(this).remove();
+                   $("#dt").text("-nothing selected-");
+                   $("#dd").text("-nothing selected-");
+                   // reset the badge counter to the current LI count
+                   $("#badge").text($("#tul li").size());
+               });
+               // set the information panel text to Item Deleted and show the panel for 2 seconds
+               $("#aPanel span").text("Item Deleted!").parent().show().delay(2000).fadeOut();
+           }
+       )).click(function(e){
+           $("#dt").text($(".t", this).text());
+           $("#dd").text($(".h", this).text());
+           }
+       ).appendTo("#tul");
+       // set the information panel text to Item Added and show the panel for 2 seconds
+       $("#aPanel span").text("Item Added!").parent().show().delay(2000).fadeOut();
+       // reset the badge counter to the current LI count
+       $("#badge").text($("#tul li").size());
+   });
+});
+   
