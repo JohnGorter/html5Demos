f3507667a18cb9c08b857ff97257307bd23a5e9c
Boss <john.gorter@gmail.com>
2014-03-05 16:47:01 +0100

    'Demos6_jQueryStep1v3'

--- App/Scripts/main.js
+++ App/Scripts/main.js
@@ -1,8 +1,26 @@
 $(function(){
+    $("#tul").keyup(function(e){
+                $("#tul").trigger('indexchange', [ "keycode", e.keyCode]);
+    });
+    $("#tul").on("indexchange", function(event, param1, param2){
+        var index = $("#tul li.selected").index();
+        var max = $("#tul li").size()-1;
+        param2 == 40 ? index++ : param2 == 38 ? index-- : index;
+        if (index < 0) { index = 0;}
+        if (index > max) { index = max;}
+        $("#tul li").removeClass("selected");
+        $($("#tul li")[index]).addClass("selected").trigger("click");
+        
+        });
+    
    // set the badge counter to the number of LI items
    $("#badge").text($("#tul li").size());
    // set click function of the panel to hide the panel
    $("#btnAlert").click(function(){ $("#aPanel").hide()});
+   $("#btnDialog").click(function(){
+        $("#t").val("");
+        $("#d").val("");
+    }); 
    // add a new todo item to the unordered list
    $("#bS").click(function() {
        $("<li>").append(
@@ -17,7 +35,7 @@ $(function(){
                $(this).parent().fadeOut("slow", function() {
                    // remove the parent LI after the LI has fully faded out
                    $(this).remove();
-                   $("#dt").text("-nothing selected-");
+                  // $("#dt").text("-nothing selected-");
                    $("#dd").text("-nothing selected-");
                    // reset the badge counter to the current LI count
                    $("#badge").text($("#tul li").size());
@@ -26,7 +44,9 @@ $(function(){
                $("#aPanel span").text("Item Deleted!").parent().show().delay(2000).fadeOut();
            }
        )).click(function(e){
-           $("#dt").text($(".t", this).text());
+           //$("#dt").text($(".t", this).text());
+           $("#tul li").removeClass("selected");
+           $(this).addClass("selected");
            $("#dd").text($(".h", this).text());
            }
        ).appendTo("#tul");
