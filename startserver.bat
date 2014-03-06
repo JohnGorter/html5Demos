8ed430c5ad6ff2cf1acf7eaa1f08bb9ff5456626
Boss <john.gorter@gmail.com>
2014-03-06 11:21:18 +0100

    'batchfileupdated'

--- startserver.bat
+++ startserver.bat
@@ -1,2 +1,5 @@
 set NODE_PATH=%AppData%\npm\node_modules
-node simpleserver.js
+
+start c:\mongodb\bin\mongod.exe
+start node simpleserver.js
+start "C:\Program Files (x86)\Google\Chrome\Application\Chrome.exe" "http://127.0.0.1:1337/todo.html"
